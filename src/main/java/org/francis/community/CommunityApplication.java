511e428818b697aac14071f265b198f41af90dc1
V22V <zairejodenis@gmail.com>
2022-11-14 11:55:55 +0800

    add @author doc

--- src/main/java/org/francis/community/CommunityApplication.java
+++ src/main/java/org/francis/community/CommunityApplication.java
@@ -3,6 +3,9 @@ package org.francis.community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author francis
+ */
 @SpringBootApplication
 public class CommunityApplication {
 
